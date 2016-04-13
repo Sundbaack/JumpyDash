406b9356c472d1aa89699d4647d3c1172d2c978e
Marcus Bertilsson <mabe.cth@gmail.com>
2016-04-13 14:27:18 +0200

    New project setup

---
+++ Jumpy Dash/desktop/src/com/jumpydash/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.jumpydash.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.jumpydash.JumpyDash;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new JumpyDash(), config);
+	}
+}
