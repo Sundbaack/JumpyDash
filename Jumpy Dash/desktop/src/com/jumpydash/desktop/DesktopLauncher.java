ae0c25f8d56b0d5dba6660e3b9bd29fee061e0df
Marcus <mabe.cth@gmail.com>
2016-04-14 16:02:27 +0200

    Render settings fix

--- Jumpy Dash/desktop/src/com/jumpydash/desktop/DesktopLauncher.java
+++ Jumpy Dash/desktop/src/com/jumpydash/desktop/DesktopLauncher.java
@@ -2,11 +2,11 @@ package com.jumpydash.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.jumpydash.JumpyDash;
+import com.jumpydash.World;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new JumpyDash(), config);
+		new LwjglApplication(new World(), config);
 	}
 }
