4fde89161290b6b93283a2cb3d3caa8868e0ccd4
Marcus <mabe.cth@gmail.com>
2016-04-14 15:54:58 +0200

    Basic render settings

--- Jumpy Dash/desktop/src/com/jumpydash/desktop/DesktopLauncher.java
+++ Jumpy Dash/desktop/src/com/jumpydash/desktop/DesktopLauncher.java
@@ -2,11 +2,13 @@ package com.jumpydash.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.jumpydash.JumpyDash;
+import com.jumpydash.World;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new JumpyDash(), config);
+		config.width = 1280;
+		config.height = 720;
+		new LwjglApplication(new World(), config);
 	}
 }
