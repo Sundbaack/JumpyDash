a40755c91f1793f3cf5d12cb57d7114244fc0e2a
Marcus <mabe.cth@gmail.com>
2016-04-17 16:18:34 +0200

    Removed Overlap2d

--- Jumpy Dash/core/src/com/jumpydash/JumpyDash.java
+++ Jumpy Dash/core/src/com/jumpydash/JumpyDash.java
@@ -3,34 +3,17 @@ package com.jumpydash;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.utils.viewport.FitViewport;
-import com.badlogic.gdx.utils.viewport.Viewport;
-import com.uwsoft.editor.renderer.SceneLoader;
-import com.uwsoft.editor.renderer.utils.ItemWrapper;
 
 public class JumpyDash extends ApplicationAdapter {
 
-	private SceneLoader sceneLoader;
-	private Viewport viewport;
-
 	@Override
 	public void create () {
-		viewport = new FitViewport(800, 480); // this should be the size of camera in WORLD units. make sure you check that in editor first.
-		sceneLoader = new SceneLoader();
-		sceneLoader.loadScene("MainScene", viewport);
 
-		/* Apply the player class to the player tile from Overlap2d
-		ItemWrapper root = new ItemWrapper(sceneLoader.getRoot());
-		Player player = new Player();
-		root.getChild("player").addScript(player);
-		*/
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(0, 0, 0, 1); // black background
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		sceneLoader.getEngine().update(Gdx.graphics.getDeltaTime()); // getting the ashley engine and updating it (it will render things with it's own render system)
-
 	}
 }
--- Jumpy Dash/core/src/com/jumpydash/Player.java
+++ Jumpy Dash/core/src/com/jumpydash/Player.java
@@ -1,28 +1,6 @@
 package com.jumpydash;
 
-import com.badlogic.ashley.core.Entity;
-import com.uwsoft.editor.renderer.scripts.IScript;
+public class Player {
 
-public class Player implements IScript {
-
-    private Entity entity;
-
-    // Initialize
-    @Override
-    public void init(Entity entity) {
-        this.entity = entity;
-    }
-
-    // Updates the actor based on time.
-    @Override
-    public void act(float delta) {
-
-    }
-
-    // Called when this screen should release all resources.
-    @Override
-    public void dispose() {
-
-    }
 }
 
