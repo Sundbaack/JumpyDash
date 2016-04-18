31d9e1657257c7a72d524fcb6779d752d061be4c
Alex Sundbäck <sundbackalex@gmail.com>
2016-04-18 14:23:31 +0200

    Added and implemented Coin
    Added implementation for Coin.java

---
+++ Jumpy Dash/core/src/com/jumpydash/Coin.java
@@ -0,0 +1,43 @@
+package com.jumpydash;
+
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+
+/**
+ * Created by alexsundback on 2016-04-18.
+ */
+
+public class Coin {
+
+    private Body body;
+    private FixtureDef fixtureDef;
+    private Fixture fixture;
+    public int valueCoin;
+
+    public Coin(Body body, int valueCoin){
+        this.body = body;
+        this.valueCoin = valueCoin;
+
+        // Create a fixture definition to apply our shape to
+        fixtureDef = new FixtureDef();
+
+        fixtureDef.shape = new PolygonShape();
+
+        // Create our fixture and attach it to the body
+        fixture = this.body.createFixture(fixtureDef);
+    }
+
+    public Body getBody() {
+        return this.body;
+    }
+
+    public Vector2 getPosition() {
+        return this.body.getPosition();
+    }
+
+    public int getValueCoin() { return this.valueCoin; }
+
+}
