39cb91e04c141b4f4e600226ea2978f598b7cabe
Alex Sundbäck <sundbackalex@gmail.com>
2016-04-29 14:35:34 +0200

    Implemented controllers and Views
    Implemented AbilityController, CoinController, PlayerController,
    AbilityView and CoinView

--- Jumpy Dash/core/src/org/chalmers/projectrolf/controller/AbilityController.java
+++ Jumpy Dash/core/src/org/chalmers/projectrolf/controller/AbilityController.java
@@ -1,7 +1,42 @@
 package org.chalmers.projectrolf.controller;
 
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import org.chalmers.projectrolf.model.Ability;
+import org.chalmers.projectrolf.model.Coin;
+import org.chalmers.projectrolf.view.AbilityView;
+import org.chalmers.projectrolf.view.CoinView;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by alexsundback on 2016-04-28.
  */
 public class AbilityController {
+
+    private BodyDef abilityBodyDef;
+    private Body abilityBody;
+    private List<Ability> abilityList;
+    private AbilityView abilityView;
+
+    public AbilityController() {
+        abilityList = new ArrayList<Ability>();
+    }
+
+    public void createObject(int x, int y, float tileWidthHeight, int mapHeight) {
+        abilityBodyDef = new BodyDef();
+        abilityBodyDef.type = BodyDef.BodyType.DynamicBody;
+        abilityBodyDef.position.set(x * tileWidthHeight / GameController.PIXELS_TO_METERS, (mapHeight - 1 - y) * 32 / GameController.PIXELS_TO_METERS);
+        abilityBody = GameController.world.createBody(abilityBodyDef);
+
+        Ability ability = new Ability(abilityBody, tileWidthHeight / GameController.PIXELS_TO_METERS);
+        abilityList.add(ability);
+        abilityView = new AbilityView(abilityList);
+    }
+
+    public AbilityView getAbilityView() {
+        return this.abilityView;
+    }
+
 }
--- Jumpy Dash/core/src/org/chalmers/projectrolf/controller/CoinController.java
+++ Jumpy Dash/core/src/org/chalmers/projectrolf/controller/CoinController.java
@@ -1,7 +1,42 @@
 package org.chalmers.projectrolf.controller;
 
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import org.chalmers.projectrolf.model.Coin;
+import org.chalmers.projectrolf.view.CoinView;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by alexsundback on 2016-04-28.
  */
-public class CoinController {
+public class CoinController extends ApplicationAdapter {
+
+    private BodyDef coinBodyDef;
+    private Body coinBody;
+    private List<Coin> coinList;
+    private CoinView coinView;
+
+    public CoinController() {
+        coinList = new ArrayList<Coin>();
+    }
+
+    public void createObject(int x, int y, float tileWidthHeight, int mapHeight) {
+        // Coin body for Box2D
+        coinBodyDef = new BodyDef();
+        coinBodyDef.type = BodyDef.BodyType.StaticBody;
+        coinBodyDef.position.set(x * tileWidthHeight / GameController.PIXELS_TO_METERS, (mapHeight - 1 - y) * 32 / GameController.PIXELS_TO_METERS);
+        coinBody = GameController.world.createBody(coinBodyDef);
+
+        Coin coin = new Coin(coinBody, 20, tileWidthHeight / GameController.PIXELS_TO_METERS);
+        coinList.add(coin);
+        coinView = new CoinView(coinList);
+    }
+
+    public CoinView getCoinView() {
+        return this.coinView;
+    }
+
 }
