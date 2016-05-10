3de2c5f8a23d06b2daee61fb227414d0ea5232c9
oscar hansson <racso.hansson@gmail.com>
2016-05-10 15:15:30 +0200

    moved bullet functionality to controller

--- Jumpy Dash/core/src/org/chalmers/projectrolf/controller/BulletController.java
+++ Jumpy Dash/core/src/org/chalmers/projectrolf/controller/BulletController.java
@@ -4,6 +4,8 @@ import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import org.chalmers.projectrolf.model.Bullet;
+import org.chalmers.projectrolf.model.JDBody;
+import org.chalmers.projectrolf.model.Player;
 import org.chalmers.projectrolf.view.BulletView;
 
 
@@ -12,13 +14,16 @@ public class BulletController extends Actor {
     private BulletView bulletView;
     private long previousFireTime;
     private Bullet bullet;
+    private static JDBody body;
     private Box2D box2D;
 
     public BulletController(Box2D box2D) {
         this.box2D = box2D;
         bulletView = new BulletView();
-        bullet = new Bullet(box2D.newBullet(PlayerController.getPosition().x,PlayerController.getPosition().y));
-
+        bullet = new Bullet();
+        body = (box2D.newBullet(PlayerController.getPosition().x,PlayerController.getPosition().y));
+        body.setLinearVelocity(new Vector2(15f,0));
+        body.setUserData(bullet);
     }
 
   /*  public void fireBullet() {
@@ -40,21 +45,25 @@ public class BulletController extends Actor {
 */
     // Remove bullets when moving out of screen
      public void updateBullets() {
-         if (((bullet.getPosition().x * box2D.getPixelsToMeters()) + 16) > (box2D.getCamera().position.x + 1280 / 2)) {
+         if (((getPosition().x * box2D.getPixelsToMeters()) + 16) > (box2D.getCamera().position.x + 1280 / 2)) {
              this.remove();
          }
      }
 
     public Vector2 getPosition(){
-        return bullet.getPosition();
+        return body.getPosition();
     }
 
+    public JDBody getJDBody(){
+        return body;
+    }
     public void act(float delta){
         updateBullets();
+        System.out.println(getPosition());
     }
 
     public void draw(Batch batch, float parentAlpha){
-        bulletView.render(batch,bullet.getPosition().x * box2D.getPixelsToMeters(), bullet.getPosition().y * box2D.getPixelsToMeters());
+        bulletView.render(batch,getPosition().x * box2D.getPixelsToMeters(), getPosition().y * box2D.getPixelsToMeters());
     }
 
     public void dispose() {
--- Jumpy Dash/core/src/org/chalmers/projectrolf/model/Bullet.java
+++ Jumpy Dash/core/src/org/chalmers/projectrolf/model/Bullet.java
@@ -1,24 +1,9 @@
 package org.chalmers.projectrolf.model;
 
-import com.badlogic.gdx.math.Vector2;
 
 public class Bullet {
 
-    private JDBody body;
-
-    public Bullet(JDBody body) {
-        this.body = body;
-        this.body.setUserData(this);
-        this.body.setLinearVelocity(new Vector2(15, 0));
+    public Bullet() {
     }
-
-    public JDBody getJDBody() {
-        return this.body;
-    }
-
-    public Vector2 getPosition() {
-        return this.body.getPosition();
-    }
-
 
 }
