a7f0c01792a0d431426857d04c535ac327f92467
oscar hansson <racso.hansson@gmail.com>
2016-05-10 14:25:24 +0200

    Bullets fire correctly

--- Jumpy Dash/core/src/org/chalmers/projectrolf/controller/BulletController.java
+++ Jumpy Dash/core/src/org/chalmers/projectrolf/controller/BulletController.java
@@ -2,65 +2,90 @@ package org.chalmers.projectrolf.controller;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import org.chalmers.projectrolf.model.Bullet;
 import org.chalmers.projectrolf.view.BulletView;
 import java.util.ArrayList;
 import java.util.List;
 
-public class BulletController {
+public class BulletController extends Actor {
 
     private BulletView bulletView;
-    private List<Bullet> bullets;
+   private List<Bullet> bullets;
     private long previousFireTime;
+    private Bullet bullet;
     private Box2D box2D;
+    public static int count = 0;
 
     public BulletController(Box2D box2D) {
         this.box2D = box2D;
         bullets = new ArrayList<Bullet>();
         bulletView = new BulletView();
+        bullet = new Bullet(box2D.newBullet(PlayerController.getPosition().x,PlayerController.getPosition().y));
+      //  bullets.add(bullet);
+      //  count++;
     }
 
-    public void fireBullet() {
+  /*  public void fireBullet() {
 
         // Cooldown
         long fireCooldown = 50;
 
         // Allow shooting if not on cooldown
-        if (System.currentTimeMillis() - previousFireTime >= fireCooldown && bullets.size() < 3) {
-
+       // if (System.currentTimeMillis() - previousFireTime >= fireCooldown && bullets.size() < 3) {
+            if(count <3){
+                System.out.println(count);
+                new Bullet(box2D.newBullet(PlayerController.getPosition().x, PlayerController.getPosition().y));
+            }
             // Reset cooldown
             previousFireTime = System.currentTimeMillis();
-            bullets.add(new Bullet(box2D.newBullet(PlayerController.getPosition().x, PlayerController.getPosition().y)));
-        }
-    }
+           // bullets.add(
 
+    }
+*/
     // Remove bullets when moving out of screen
-    public void updateBullets() {
-        for (int i = 0; i < bullets.size(); i++) {
-            if (((bullets.get(i).getPosition().x * box2D.getPixelsToMeters()) + 16) > (box2D.getCamera().position.x + 1280 / 2)) {
-                bullets.remove(i);
-            }
-        }
+     public void updateBullets() {
+         if (((bullet.getPosition().x * box2D.getPixelsToMeters()) + 16) > (box2D.getCamera().position.x + 1280 / 2)) {
+             this.remove();
+         }
+     }
+
+    public Vector2 getPosition(){
+        return bullet.getPosition();
     }
 
-    public void update(SpriteBatch batch) {
+ /*   public void update(SpriteBatch batch) {
         handleInput();
-        updateBullets();
+     // updateBullets();
         render(batch);
     }
-
-    public void render(SpriteBatch  batch) {
-        for (Bullet b : bullets) {
-            bulletView.render(b.getPosition().x * box2D.getPixelsToMeters(), b.getPosition().y * box2D.getPixelsToMeters(), batch);
-        }
+*/
+    public void act(float delta){
+        //handleInput();
+        updateBullets();
     }
+
+    public void draw(Batch batch, float parentAlpha){
+        bulletView.render(batch,bullet.getPosition().x * box2D.getPixelsToMeters(), bullet.getPosition().y * box2D.getPixelsToMeters());
+       // fireBullet();
+
+    }
+  /*  public void render(SpriteBatch  batch) {
+        for (Bullet b : bullets) {
+
+        }
+    }*/
 
     public void handleInput() {
         if (Gdx.input.isKeyJustPressed(Input.Keys.F)) {
-            fireBullet();
+            //fireBullet();
         }
     }
+
 
     public void dispose() {
         bulletView.dispose();
--- Jumpy Dash/core/src/org/chalmers/projectrolf/controller/JumpyDash.java
+++ Jumpy Dash/core/src/org/chalmers/projectrolf/controller/JumpyDash.java
@@ -2,6 +2,7 @@ package org.chalmers.projectrolf.controller;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
