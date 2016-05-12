15e7d22f7d0b32a9482f2797b5c26db3048c3e24
Alex Sundbäck <sundbackalex@gmail.com>
2016-05-12 14:58:56 +0200

    Moved logic from Controller to Class

--- Jumpy Dash/core/src/org/chalmers/projectrolf/controller/AbilityController.java
+++ Jumpy Dash/core/src/org/chalmers/projectrolf/controller/AbilityController.java
@@ -1,26 +1,21 @@
 package org.chalmers.projectrolf.controller;
 
 import com.badlogic.gdx.graphics.g2d.Batch;
-import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import org.chalmers.projectrolf.model.Ability;
 import org.chalmers.projectrolf.physics.IBox2D;
-import org.chalmers.projectrolf.physics.IJDBody;
 import org.chalmers.projectrolf.view.AbilityView;
 
 public class AbilityController extends Actor{
 
     private AbilityView abilityView;
     private IBox2D box2D;
-    private IJDBody jdBody;
     private Ability ability;
 
     public AbilityController(IBox2D box2D, int x, int y, int mapHeight) {
         this.box2D = box2D;
-        ability = new Ability();
+        ability = new Ability(box2D.newDynamic(x, y, mapHeight));
         abilityView = new AbilityView();
-        jdBody = box2D.newDynamic(x, y, mapHeight);
-        jdBody.setUserData(ability);
     }
 
     public void act(float Delta){
@@ -29,12 +24,8 @@ public class AbilityController extends Actor{
 
     @Override
     public void draw(Batch batch, float parentAlpha) {
-        abilityView.render(batch, getPosition().x * box2D.getPixelsToMeters(), getPosition().y * box2D.getPixelsToMeters());
+        abilityView.render(batch, ability.getPosition().x * box2D.getPixelsToMeters(), ability.getPosition().y * box2D.getPixelsToMeters());
 
-    }
-
-    public Vector2 getPosition() {
-        return jdBody.getPosition();
     }
 
     public void dispose() {
--- Jumpy Dash/core/src/org/chalmers/projectrolf/controller/CoinController.java
+++ Jumpy Dash/core/src/org/chalmers/projectrolf/controller/CoinController.java
@@ -1,26 +1,21 @@
 package org.chalmers.projectrolf.controller;
 
 import com.badlogic.gdx.graphics.g2d.Batch;
-import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import org.chalmers.projectrolf.model.Coin;
 import org.chalmers.projectrolf.physics.IBox2D;
-import org.chalmers.projectrolf.physics.IJDBody;
 import org.chalmers.projectrolf.view.CoinView;
 
 public class CoinController extends Actor {
 
     private CoinView coinView;
     private IBox2D box2D;
-    private IJDBody jdBody;
     private Coin coin;
 
     public CoinController(IBox2D box2D, int x, int y, int mapHeight) {
         this.box2D = box2D;
         coinView = new CoinView();
-        coin = new Coin(1);
-        jdBody = box2D.newStatic(x, y, mapHeight, false);
-        jdBody.setUserData(coin);
+        coin = new Coin(1, box2D.newStatic(x, y, mapHeight, false));
     }
 
     public void act(float Delta){
@@ -29,13 +24,11 @@ public class CoinController extends Actor {
 
     @Override
     public void draw(Batch batch, float parentAlpha) {
-        coinView.render(batch, getPosition().x * box2D.getPixelsToMeters(), getPosition().y * box2D.getPixelsToMeters());
+        coinView.render(batch, coin.getPosition().x * box2D.getPixelsToMeters(), coin.getPosition().y * box2D.getPixelsToMeters());
 
     }
 
-    public Vector2 getPosition() {
-        return jdBody.getPosition();
-    }
+
 
     public void dispose() {
         coinView.dispose();
