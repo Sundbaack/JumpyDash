6552c3105f274d92ac9fe5392793d00aa234fc63
Alex Sundbäck <sundbackalex@gmail.com>
2016-05-16 16:33:47 +0200

    Added and implemented Spike
    Implemented Controller, View and Class

---
+++ Jumpy Dash/core/src/org/chalmers/jumpydash/model/Spike.java
@@ -0,0 +1,21 @@
+package org.chalmers.jumpydash.model;
+
+import org.chalmers.jumpydash.physics.IJDBody;
+
+import javax.vecmath.Vector2f;
+
+/**
+ * Created by alexsundback on 2016-05-16.
+ */
+public class Spike {
+
+    private IJDBody jdBody;
+
+    public Spike(IJDBody jdBody) {
+        this.jdBody = jdBody;
+        this.jdBody.setUserData(this);
+    }
+
+    public Vector2f getPosition() { return jdBody.toVector2f(jdBody.getPosition()); }
+
+}
