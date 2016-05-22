4baea67113c6209deb299e4ca91a73016c1d57b8
Marcus Bertilsson <mabe.cth@gmail.com>
2016-05-22 15:26:26 +0200

    Added test dir

---
+++ Jumpy Dash/core/src/test/java/org/chalmers/jumpydash/test/PlayerTest.java
@@ -0,0 +1,18 @@
+package org.chalmers.jumpydash.test;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class PlayerTest {
+
+    @Test
+    public void jump() throws Exception {
+
+    }
+
+    @Test
+    public void move() throws Exception {
+
+    }
+
+}
