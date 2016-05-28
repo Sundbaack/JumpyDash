675bfe98f8c9c29fee1803e21b102e155ece601a
Alex Sundbäck <sundbackalex@gmail.com>
2016-05-28 16:13:10 +0200

    Implemented back to menu in PauseView

--- Jumpy Dash/core/src/main/org/chalmers/jumpydash/controller/screen/PauseScreen.java
+++ Jumpy Dash/core/src/main/org/chalmers/jumpydash/controller/screen/PauseScreen.java
@@ -2,7 +2,9 @@ package org.chalmers.jumpydash.controller.screen;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import org.chalmers.jumpydash.view.screen.PauseView;
 
 public class PauseScreen extends JDScreen {
@@ -15,6 +17,17 @@ public class PauseScreen extends JDScreen {
         this.stage = stage;
         this.uiStage = uiStage;
         this.pauseView = new PauseView(uiStage);
+
+        setListeners();
+    }
+
+    private void setListeners() {
+        pauseView.getMenuButton().addListener(new ClickListener() {
+            public void clicked(InputEvent event, float x, float y) {
+                // Switch to menu
+                ScreenManager.getInstance().initMenu(stage, uiStage);
+            }
+        });
     }
 
     private void handleInput() {
